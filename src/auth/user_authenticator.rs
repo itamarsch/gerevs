778d3c510dbe7eee1cba172fdfbc690ae5341643
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-07-25 15:13:01 +0300

    Username password docs

--- src/auth/user_authenticator.rs
+++ src/auth/user_authenticator.rs
@@ -1,3 +1,33 @@
+//! # User Authentication Module
+//!
+//! This module provides structures and traits for implementing username and password authentication
+//! in the SOCKS5 protocol. It includes the `UsernamePasswordAuthenticator` struct for performing
+//! the authentication and the `UserAuthenticator` trait for validating user credentials.
+//!
+//! ## Example
+//!
+//! ```rust
+//! struct SimpleUserAuthenticator;
+//!
+//! impl UserAuthenticator for SimpleUserAuthenticator {
+//!     type Credentials = ();
+//!
+//!     async fn authenticate_user(
+//!         &mut self,
+//!         user: User,
+//!     ) -> io::Result<Option<Self::Credentials>> {
+//!         if user.username == "admin" && user.password == "password" {
+//!             Ok(Some(()))
+//!         } else {
+//!             Ok(None)
+//!         }
+//!     }
+//! }
+//!
+//! let user_authenticator = SimpleUserAuthenticator;
+//! let auth = UsernamePasswordAuthenticator::new(user_authenticator);
+//! ```
+
 use std::{
     future::Future,
     io::{self, ErrorKind},
@@ -12,6 +42,7 @@ use super::Authenticator;
 
 const USER_PASSWORD_VERSION: u8 = 0x01;
 
+/// Represents a user with a username and password.
 #[derive(Debug)]
 pub struct User {
     pub username: String,
@@ -24,15 +55,26 @@ enum AuthStatus {
     Failure = 0x01,
 }
 
+/// The `UserAuthenticator` trait defines the functionality for validating (authenticating) user credentials.
 pub trait UserAuthenticator {
     type Credentials;
 
+    /// Validates the provided user credentials and returns an optional `Credentials` value if
+    /// the validation (authentication) is successful.
+    ///
+    /// - `user`: The user credentials to authenticate.
+    /// - Returns a future that resolves to `io::Result<Option<Self::Credentials>>>`:
+    ///   - `Ok(Some(credentials))`: Authentication was successful, and credentials are provided.
+    ///   - `Ok(None)`: Authentication failed.
+    ///   - `Err(error)`: An error occurred during the authentication process.
     fn authenticate_user(
         &mut self,
         user: User,
     ) -> impl Future<Output = io::Result<Option<Self::Credentials>>> + Send;
 }
 
+/// The `UsernamePasswordAuthenticator` struct handles the username and password authentication process
+/// in the SOCKS5 protocol.
 pub struct UsernamePasswordAuthenticator<U>
 where
     U: UserAuthenticator,
@@ -48,6 +90,9 @@ where
 {
     type Credentials = U::Credentials;
 
+    /// Selects the `UsernamePassword` authentication method if it is present in the provided list
+    /// of methods. If not, it selects `NoAcceptableMethods` to indicate that no suitable
+    /// authentication method is available.
     fn select_method(&self, methods: &[AuthMethod]) -> AuthMethod {
         if methods.contains(&AuthMethod::UsernamePassword) {
             AuthMethod::UsernamePassword
@@ -56,6 +101,7 @@ where
         }
     }
 
+    /// Performs the authentication process using the username and password method.
     async fn authenticate(
         &mut self,
         conn: &mut T,
@@ -82,6 +128,10 @@ impl<U> UsernamePasswordAuthenticator<U>
 where
     U: UserAuthenticator,
 {
+    /// Creates a new `UsernamePasswordAuthenticator` with the provided user authenticator.
+    ///
+    /// - `user_authenticator`: The user authenticator that implements the `UserAuthenticator` trait.
+    /// - Returns a new `UsernamePasswordAuthenticator`.
     pub fn new(user_authorizer: U) -> UsernamePasswordAuthenticator<U> {
         UsernamePasswordAuthenticator {
             user_authenticator: user_authorizer,
